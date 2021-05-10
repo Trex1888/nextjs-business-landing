e16d591bcabf6349e0cf62f63beaf28a4159e8d1
Trex1888 <harpua123@gmail.com>
2021-05-10 04:30:49 -0500

    6th commit

--- src/sections/workflow.js
+++ src/sections/workflow.js
@@ -2,7 +2,6 @@
 import { jsx } from "theme-ui";
 import { Container, Grid, Box, Heading, Text } from "theme-ui";
 import SectionHeader from "components/section-header";
-
 import PatternBG from "assets/patternBG.png";
 import ArrowOdd from "assets/arrowOdd.svg";
 import ArrowEven from "assets/arrowEven.svg";
